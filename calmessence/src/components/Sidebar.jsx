2edb48e898e3651cd80bb6e6eec4b24db5bbfcb0
Nikulkumar Popatbhai Kukadiya <nk865270@dal.ca>
2024-07-30 18:54:08 -0300

    - Live chat dynamic url change.

--- calmessence/src/components/Sidebar.jsx
+++ calmessence/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-const Sidebar = ({ experts, chats, setSelectedChat, fetchMessages, userId }) => (
+const Sidebar = ({ experts, chats, setSelectedChat, fetchMessages, userId, user_name,  createChatRequest }) => (
   <div className="w-full md:w-1/4 h-full bg-gray-100 p-4 overflow-y-auto">
     <div className="mb-4">
       <h2 className="text-xl font-semibold mb-2">Chats</h2>
@@ -25,6 +25,9 @@ const Sidebar = ({ experts, chats, setSelectedChat, fetchMessages, userId }) => 
         {experts.filter(expert => !chats.some(chat => chat.user_id2 === expert.email)).map((expert) => (
           <div
             key={expert.email}
+            onClick={() => {
+              createChatRequest(userId, expert.email, user_name, expert.full_name, ''); // Add necessary params
+            }}
             className="p-2 bg-white rounded-lg mb-2 shadow-md cursor-pointer hover:bg-gray-200"
           >
             <h3 className="text-lg font-medium">{expert.full_name}</h3>
